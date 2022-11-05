19d8513cba6e3062e181fd5fb98fe1183d7a2800
Patrick Zhu <patrick140814@outlook.com>
2022-11-05 23:51:02 +0800

    添加Library，测试添加项目文件。

---
+++ TickOffListLibrary/Models/Habit.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlTypes;
+
+namespace TickOffListLibrary.Models; 
+
+public class Habit {
+    [SQLite.Column("id")]
+    public int Id { get; set; }
+
+    [SQLite.Column("title")]
+    public string title { get; set; } = string.Empty;
+
+    [SQLite.Column("describe")]
+    public string describe { get; set; } = string.Empty;
+
+    [SQLite.Column("iconName")]
+    public string iconName { get; set; } = string.Empty;
+
+    [SQLite.Column("days")]
+    public string days { get; set; } = string.Empty;
+
+    [SQLite.Column("quantity")]
+    public int quantity { get; set; }
+}
