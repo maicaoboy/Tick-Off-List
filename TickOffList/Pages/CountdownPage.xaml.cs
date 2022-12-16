7dfb4fb131ea9d997d4feaca0362722f76f7010f
tao-jinglong <9406353+tao-jinglong@user.noreply.gitee.com>
2022-12-16 20:19:16 +0800

    删除无效代码

--- TickOffList/Pages/CountdownPage.xaml.cs
+++ TickOffList/Pages/CountdownPage.xaml.cs
@@ -4,25 +4,5 @@ namespace TickOffList.Pages;
 public partial class CountdownPage : ContentPage {
     public CountdownPage() {
         InitializeComponent();
-
-        // InitializePickers();
     }
-
-    // private void InitializePickers() {
-    //     for (var i = 0; i <= 99; i++)
-    //         if (i <= 9) {
-    //             HourPicker.Items.Add($"0{i}");
-    //             MinutePicker.Items.Add($"0{i}");
-    //             SecondPicker.Items.Add($"0{i}");
-    //         } else if (i <= 59) {
-    //             MinutePicker.Items.Add($"{i}");
-    //             SecondPicker.Items.Add($"{i}");
-    //         } else {
-    //             HourPicker.Items.Add($"{i}");
-    //         }
-    //     
-    //     HourPicker.SelectedItem = "00";
-    //     MinutePicker.SelectedItem = "00";
-    //     SecondPicker.SelectedItem = "00";
-    // }
 }
